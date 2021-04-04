4bc75ed49ac8a7184da0982e2b4e0c356c2b468f
FMinister <56680320+FMinister@users.noreply.github.com>
2021-04-04 23:48:52 +0200

    Initial commit

---
+++ Sudoku_Solver_Backtracking/Programm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sudoku_Solver_Backtracking
+{
+    public static class Programm
+    {
+        public static void Main()
+        {
+            List<List<int>> sudoku = new List<List<int>>();
+            sudoku.Add(new List<int> { 5, 0, 1, 0, 7, 0, 3, 9, 0 });
+            sudoku.Add(new List<int> { 3, 0, 0, 1, 0, 0, 2, 0, 0 });
+            sudoku.Add(new List<int> { 0, 0, 2, 6, 5, 0, 0, 7, 1 });
+            sudoku.Add(new List<int> { 1, 6, 4, 9, 3, 0, 0, 0, 0 });
+            sudoku.Add(new List<int> { 0, 0, 0, 4, 0, 0, 0, 1, 0 });
+            sudoku.Add(new List<int> { 0, 0, 0, 0, 6, 1, 0, 3, 0 });
+            sudoku.Add(new List<int> { 0, 3, 0, 0, 0, 4, 9, 0, 8 });
+            sudoku.Add(new List<int> { 0, 0, 5, 0, 0, 8, 1, 4, 3 });
+            sudoku.Add(new List<int> { 0, 1, 8, 0, 0, 0, 5, 2, 0 });
+
+            bool solved = Solver.SolveSudoku(sudoku);
+
+            Console.WriteLine(solved);
+        }
+    }
+}
---
+++ Sudoku_Solver_Backtracking/Solver.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sudoku_Solver_Backtracking
+{
+    public static class Solver
+    {
+        public static bool SolveSudoku(List<List<int>> sudoku)
+        {
+            Tuple<int, int> rowAndCol;
+            rowAndCol = findNextEmpty(sudoku);
+
+            if (rowAndCol.Item1 == -1)
+            {
+                printSudoku(sudoku);
+                return true;
+            }
+
+            for (int guess = 1; guess < 10; guess++)
+            {
+                if (guessIsValid(sudoku, guess, rowAndCol))
+                {
+                    sudoku[rowAndCol.Item1][rowAndCol.Item2] = guess;
+                    if (SolveSudoku(sudoku))
+                    {
+                        return true;
+                    }
+                }
+
+                sudoku[rowAndCol.Item1][rowAndCol.Item2] = 0;
+            }
+
+            return false;
+        }
+
+        private static void printSudoku(List<List<int>> sudoku)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                string text = "";
+                for (int col = 0; col < 9; col++)
+                {
+                    text = text + sudoku[row][col] + " ";
+                }
+
+                Console.WriteLine(text);
+            }
+        }
+
+        private static Tuple<int, int> findNextEmpty(List<List<int>> sudoku)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    if (sudoku[row][col] == 0)
+                    {
+                        return new Tuple<int, int>(row, col);
+                    }
+                }
+            }
+
+            return new Tuple<int, int>(-1, -1);
+        }
+
+        private static bool guessIsValid(List<List<int>> sudoku, int guess, Tuple<int, int> rowAndCol)
+        {
+            List<int> rowValues = sudoku[rowAndCol.Item1];
+            if (rowValues.Contains(guess))
+            {
+                return false;
+            }
+
+            List<int> columnValues = new List<int>();
+            for (int row = 0; row < 9; row++)
+            {
+                columnValues.Add(sudoku[row][rowAndCol.Item2]);
+            }
+            if (columnValues.Contains(guess))
+            {
+                return false;
+            }
+
+            int rowStart = (rowAndCol.Item1 / 3) * 3;
+            int colStart = (rowAndCol.Item2 / 3) * 3;
+            for (int r = rowStart; r < rowStart + 3; r++)
+            {
+                for (int c = colStart; c < colStart + 3; c++)
+                {
+                    if (guess == sudoku[r][c])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
